92a293dccb46baa2604087e86fbe180e653f6f6c
Pavel Valodzka <pavel@valodzka.name>
2011-10-26 14:42:21 +0400

    add join chat

--- ext/purple_ruby_ext.c
+++ ext/purple_ruby_ext.c
@@ -714,6 +714,27 @@ static VALUE account_find_conversation(VALUE self, VALUE name)
   return Qnil;
 }
 
+static VALUE account_join_chat(VALUE self, VALUE name)
+{
+  PurpleAccount *account;
+  Data_Get_Struct(self, PurpleAccount, account);
+  GHashTable *data;
+
+  if (purple_account_is_connected(account)) {
+    data = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
+
+    g_hash_table_insert(data, "chat_id", StringValueCStr(name));
+
+    serv_join_chat(purple_account_get_connection(account), data);
+
+    g_free(data);
+
+    return Qtrue;
+  }
+
+  return Qfalse;
+}
+
 static VALUE account_create_chat(VALUE self, VALUE name)
 {
   PurpleAccount *account;
@@ -1110,6 +1131,7 @@ void Init_purple_ruby_ext()
   rb_define_method(cAccount, "protocol_name", protocol_name, 0);
   rb_define_method(cAccount, "find_conversation", account_find_conversation, 1);
   rb_define_method(cAccount, "create_chat", account_create_chat, 1);
+  rb_define_method(cAccount, "join_chat", account_join_chat, 1);
   rb_define_method(cAccount, "get_bool_setting", get_bool_setting, 2);
   rb_define_method(cAccount, "set_bool_setting", set_bool_setting, 2);
   rb_define_method(cAccount, "get_int_setting", get_int_setting, 2);
